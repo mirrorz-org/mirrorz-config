9e5a978fbc5b4155def3ac8ec5fa6c0bd7314d49
Zenithal <i@zenithal.me>
2021-08-13 01:37:45 +0800

    Use config.json instead of hardencode

--- upstream.ts
+++ upstream.ts
@@ -1,68 +1,9 @@
-import { Parser } from "../parser";
-import lzu from "./../parser/lzu";
-import nju from "./../parser/nju";
-import neusoft from "./../parser/neusoft";
-import hust from "./../parser/hust";
-import tuna from "./../parser/tuna";
-import bfsu from "./../parser/bfsu";
-import nano from "./../parser/nano";
-import neo from "./../parser/neo";
-import hit from "./../parser/hit";
-import cqu from "./../parser/cqu";
-import xjtu from "./../parser/xjtu";
-//const neu = require("./../parser/neu");
-import nyist from "./../parser/nyist";
-import scau from "./../parser/scau";
-import zju from "./../parser/zju";
-import pku from "./../parser/pku";
-import byrio from "./../parser/byrio";
-import cqupt from "./../parser/cqupt";
-import ynuosa from "./../parser/ynuosa";
-import xtom_hk from "../parser/xtom-hk";
-import xtom from "../parser/xtom";
-import xtom_de from "../parser/xtom-de";
-import xtom_nl from "../parser/xtom-nl";
-import xtom_ee from "../parser/xtom-ee";
-import njupt from "../parser/njupt";
-import wsyu from "../parser/wsyu";
+import parsers, { Parser } from "../parser";
+import config from "./config.json";
 
-const upstreams: (string | Parser)[] = [
-  "https://status.tuna.wiki/mirrorz/static/opentuna.json",
-  "https://mirrors.ustc.edu.cn/static/json/mirrorz.json",
-  "https://mirror.sjtu.edu.cn/mirrorz/siyuan.json",
-  "https://mirror.sjtu.edu.cn/mirrorz/zhiyuan.json",
-  "https://mirrors.dgut.edu.cn/static/mirrorz.json",
-  "https://mirrors.sustech.edu.cn/mirrorz/mirrorz.json",
-  "https://iptv.uestc.edu.cn/mirrors/mirrorz.json",
-  "https://mirrors.nwafu.edu.cn/api/mirrorz/info.json",
-  "https://mirrorz.org/static/json/legacy/bjtu.json",
-  "https://mirrorz.org/static/json/legacy/tongji.json",
-  "https://mirrorz.org/static/json/legacy/neu.json",
-  lzu,
-  nju,
-  neusoft,
-  hust,
-  tuna,
-  bfsu,
-  nano,
-  neo,
-  hit,
-  cqu,
-  nyist,
-  scau,
-  zju,
-  pku,
-  byrio,
-  cqupt,
-  ynuosa,
-  xjtu,
-  xtom_hk,
-  xtom,
-  xtom_de,
-  xtom_nl,
-  xtom_ee,
-  njupt,
-  wsyu,
-];
+const upstreams: (string | Parser)[] =
+  config.upstream_legacy.map((e) => config.url + '/static/json/legacy/' + e + '.json')
+    .concat(config.upstream_mirrors)
+    .concat(config.upstream_parser.map((e) => parsers[e]));
 
 export default upstreams;
