a922521b333b78f092f011e2bcee8f5c11419b2b
Zenithal <i@zenithal.me>
2021-08-15 05:30:32 +0800

    Change config.json/mirrors format

--- mirrors.js
+++ mirrors.js
@@ -1,4 +1,4 @@
 const config = require('./config.json')
 module.exports =
   config.mirrors_legacy.map((e) => config.url + '/static/json/legacy/' + e + '.json')
-    .concat(Object.keys(config.mirrors))
+    .concat(Object.values(config.mirrors))
