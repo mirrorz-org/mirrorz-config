65c390351450708961d0623090ca9d983da1fffc
taoky <me@taoky.moe>
2026-07-16 00:02:14 +0800

    Move cname.json to mirrorz-config

--- parser/utils.js
+++ parser/utils.js
@@ -1,10 +1,6 @@
 const config = require('../config.json');
-var cname;
+const cname = require('../cname.json');
 
 exports.cname = async function() {
-  if (!cname) {
-    const data = await fetch(config.url + "/static/json/cname.json");
-    cname = await data.json();
-  }
   return (name) => { return (name in cname) ? cname[name] : name; };
 };
