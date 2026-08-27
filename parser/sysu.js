22cd27efc718d11c7879b1cd586c4e2c73b0c71a
tedaimeng <tdm@tedaimeng.cn>
2026-08-28 00:41:34 +0800

    fix: delete invalid repo in sysu mirror

--- parser/sysu.js
+++ parser/sysu.js
@@ -3,7 +3,9 @@ const siteData = require("./sites/sysu.json");
 
 module.exports = async function () {
   const site = JSON.parse(JSON.stringify(siteData));
-  const mirrors = await tunasync("https://mirror.sysu.edu.cn/status");
+  const allMirrors = await tunasync("https://mirror.sysu.edu.cn/status");
+  // disable repositories that never sync successfully
+  const mirrors = allMirrors.filter(m => !m.status.startsWith('F') || m.status.includes('O'));
   return {
     site,
     mirrors,
