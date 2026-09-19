6b8d304de014a409b4e409def3ab3225e51d3455
ht4w5 <ht4w5x@gmail.com>
2026-09-19 08:59:43 +0800

    xjtu: update mirrors and isoinfo endpoint urls

--- sites/xjtu/xjtu.js
+++ sites/xjtu/xjtu.js
@@ -4,9 +4,9 @@ const siteData = require("./xjtu.json");
 
 module.exports = async function () {
   const site = JSON.parse(JSON.stringify(siteData));
-  let mirrors = await tunasync("https://mirrors.xjtu.edu.cn/.well-known/mirrorz-org-mirrors.json");
+  let mirrors = await tunasync("https://mirrors.xjtu.edu.cn/api/mirrors.json");
   mirrors = mirrors.map((item) => ({ ...item, url: item.url + "/" }));
-  const info = await isoinfo("https://mirrors.xjtu.edu.cn/.well-known/mirrorz-org-isoinfo.json");
+  const info = await isoinfo("https://mirrors.xjtu.edu.cn/api/downloads.json");
 
   return {
     site,
